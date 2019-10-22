18b3608952a89a59ee1eb9b90309bb0f9d03a941
Heba Fayad <heba.fayad.dobai@gmail.com>
2019-10-22 03:59:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ main.remove();
 let newheader =document.getElementByTagName("body");
  newheader.innerHTML=<h1 ></h1>
 document.main.appendChild(newheader);
+main.remove();
+let newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory")
+document.body.appendChild(newHeader);
+let element = document.querySelector("h1#victory");
+element.innerHTML = 'Firas is the champion'

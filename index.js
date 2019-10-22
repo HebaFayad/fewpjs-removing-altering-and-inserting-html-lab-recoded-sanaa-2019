b74ce5a2af92eaadd73dfe18cd7e45a01b82bceb
Heba Fayad <heba.fayad.dobai@gmail.com>
2019-10-22 04:00:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Write your code here!
 main.remove();
 let newheader =document.getElementByTagName("body");
- newheader.innerHTML=<h1 ></h1>
-document.main.appendChild(newheader);
-main.remove();
+ newheader.innerHTML=<h1 id="victory" ></h1>;
+
+/*main.remove();
 let newHeader = document.createElement('h1');
 newHeader.setAttribute("id", "victory")
 document.body.appendChild(newHeader);
 let element = document.querySelector("h1#victory");
-element.innerHTML = 'Firas is the champion'
+element.innerHTML = 'Firas is the champion'*/
